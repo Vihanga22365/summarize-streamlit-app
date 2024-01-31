0ce56a78ae878ed578cb2f676b5dc13640a4ede2
Vihanga Mihiranga <50793729+Vihanga22365@users.noreply.github.com>
2024-01-31 13:09:08 +0530

    Create UI

--- Hello.py
+++ Hello.py
@@ -1,51 +1,56 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
 
-LOGGER = get_logger(__name__)
+# Set the page to wide mode
+st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
 
+# Title for your app
+st.title('Text Processing App')
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+st.markdown("""
+<style>
+    /* Target the buttons inside the Streamlit app to expand to full width */
+    .stButton>button {
+        width: 100%;
+    }
+            
+    [data-testid="collapsedControl"] {
+        display: none
+    }
+</style>
+""", unsafe_allow_html=True)
 
-    st.write("# Welcome to Streamlit! 👋")
+# Creating columns for the layout
+col1, col2, col3 = st.columns([3, 1, 3])
 
-    st.sidebar.success("Select a demo above.")
+# First column for user input text area
+with col1:
+    user_input = st.text_area("Enter Text Here", height=300)
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+# Second column for buttons
+with col2:
+    st.write("")  # This is just to add some space at the top
+    summary_btn = st.button("Summary", key="summary_btn")
+    classify_btn = st.button("Classify", key="classify_btn")
 
+# Third column for the result text area
+with col3:
+    result = st.text_area("Result", height=300)
 
-if __name__ == "__main__":
-    run()
+# Functions for summary and classification - Replace with your own functions
+def summarize(text):
+    # Your summary logic goes here
+    return "Summarized text"
+
+def classify(text):
+    # Your classification logic goes here
+    return "Classification result"
+
+# When the 'Summary' button is pressed
+if summary_btn:
+    summary_result = summarize(user_input)
+    result.text_area("Result", value=summary_result, height=300, key='result')
+
+# When the 'Classify' button is pressed
+if classify_btn:
+    classification_result = classify(user_input)
+    result.text_area("Result", value=classification_result, height=300, key='result')
